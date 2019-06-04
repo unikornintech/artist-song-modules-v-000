82b2f36031870104f983d56f63b52fb438aa472c
Lauren Days <lmchambers0420@gmail.com>
2019-06-04 23:29:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,9 +13,9 @@ class Song
 
   @@songs = []
 
-  # def initialize
-  #  @@songs << self
-  # end
+   def initialize
+    self.class.all << self
+   end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
